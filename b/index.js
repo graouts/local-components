0fe83ffb53ad06e913da58ab0c6cd7dbc7345efc
Antoine Quint <antoine@graougraou.com>
2015-01-05 17:17:39 -0800

    Three distinct components with cross-dependencies using the .locals property.

---
+++ b/index.js
@@ -0,0 +1,4 @@
+var a = require("a");
+console.log("b required a", a);
+
+module.exports = "b";
---
+++ c/index.js
@@ -0,0 +1,6 @@
+var a = require("a");
+var b = require("b");
+console.log("c required a", a);
+console.log("c required b", b);
+
+module.exports = "c";
